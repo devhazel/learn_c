71a05f3b617706551b19e84863cdc4d0b12126ce
devhazel <hazelpark.dev@gmail.com>
2021-05-21 00:16:58 +0900

    00. hello world

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+/*
+ 아래 헤더파일을 이 프로그램에 포함하지 않은 경우 [use of undeclared identifier 'printf'] 에러가 발생한다.
+ 왜냐하면 printf()라는 output 함수는 standard-input-output 헤더파일에 정의되어 있기 때문이다.
+ */  
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
